c529c78139e9607e7286c6c4dcf3a7af71a86f28
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-05-29 19:19:59 -0400

    Remove unused imports

--- comebacksunday/polls/views.py
+++ comebacksunday/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
 from .models import Question
 
 # Create your views here.
