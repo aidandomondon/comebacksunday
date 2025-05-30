69189c04bc4fd2cce1348727b0ed62ef33e72113
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-05-30 19:57:25 -0400

    Fix syntax error with unused import

--- comebacksunday/posts/views.py
+++ comebacksunday/posts/views.py
@@ -3,7 +3,6 @@ from django.db.utils import IntegrityError
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .models import ExtendedUser, Post
-from django.template.loader import r 
 
 def following(request, username) -> HttpResponse:
     """
