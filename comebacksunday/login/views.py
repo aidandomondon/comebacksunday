7c3908e1dc23df83335315fc8eaea1c84e351d70
Python Anywhere VM <pythonanywhere@comebacksunday.com>
2025-06-02 17:57:22 +0000

    Removes polls app directory and old login app directory

--- comebacksunday/login/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def view(request) -> HttpResponse:
-    return HttpResponse("Login process not yet configured; come back later :(")
