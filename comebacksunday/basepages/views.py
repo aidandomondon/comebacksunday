cf04e7bee64a62d471af803523a0169928037085
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-06-30 21:34:46 -0400

    Removes Log in and Register options from welcome page if user logged in

--- comebacksunday/basepages/views.py
+++ comebacksunday/basepages/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def welcome_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'basepages/welcome_page.html')
+    return render(
+        request, 
+        'basepages/welcome_page.html', 
+        context={
+            'user': request.user
+        }
+    )
